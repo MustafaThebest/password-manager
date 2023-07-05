c9ffc355da45cbb6c9b7b0c6f9510a956d01e343
Mustafa <45076611+MustafaThebest@users.noreply.github.com>
2023-07-05 16:03:38 +0200

    Passwords List Component Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PasswordsListComponent } from './passwords-list/passwords-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PasswordsListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/passwords-list/passwords-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-passwords-list',
+  templateUrl: './passwords-list.component.html',
+  styleUrls: ['./passwords-list.component.css']
+})
+export class PasswordsListComponent {
+
+}
